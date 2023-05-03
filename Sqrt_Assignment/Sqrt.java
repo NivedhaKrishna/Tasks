6087dbcbf3ea24b34581468a28bf3d2a6027ac33
NivedhaKrishna <132180496+NivedhaKrishna@users.noreply.github.com>
2023-05-03 21:29:14 +0530

    Add files via upload

---
+++ Sqrt_Assignment/Sqrt.java
@@ -0,0 +1,14 @@
+package Sqrt_Assignment;
+
+public class Sqrt {
+	public static void main(String args[]) throws Exception {
+		int input = 4;
+		int result = (int) Math.sqrt(input);
+		System.out.println("The Sqrt of 4 is: "+result);
+		
+		int inputValue = 8;
+		double finalResult = (double) Math.sqrt(inputValue);
+		System.out.println("The Sqrt of 8 is: "+finalResult);
+
+			}
+}
